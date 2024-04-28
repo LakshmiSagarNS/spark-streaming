03533531c4e9e4fc2cb1db7e324abf1318ca15e2
LakshmiSagarNS <nslakshmisagar12@gmail.com>
2024-04-28 09:29:34 +0530

    converted csv values to str

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,13 +1,12 @@
 from pyspark.sql import SparkSession ,functions as F
 from pyspark.conf import SparkConf
-from pyspark.sql.types import StructType,StructField,StringType,IntegerType
-
-
-
+from pyspark.sql.functions import col,concat_ws
 
 
 conf = SparkConf()
 conf.set("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1")
+
+#start of kafka session
 
 spark = SparkSession \
     .builder \
@@ -15,6 +14,7 @@ spark = SparkSession \
     .master("local") \
     .config(conf=conf) \
     .getOrCreate()
+
 #kafka configuration
 
 bootstrap_servers = "localhost:9092"
@@ -22,43 +22,16 @@ topic = "test"
 
 #defining the csv file path
 
-file_path = "data.csv"
+file_path = "amazon.csv"
 df = spark.read.format("csv").option("header", True).load(file_path)
 
-# try:
-#   df = spark.read.format("csv").option("header", True).load(file_path)
-#   df = df.select(F.to_json(F.struct(*df.columns)).alias("value"))
-# except:  # Catch any errors during reading with header
-#   pass
+#converting csv file to string
 
-# if not df:
-#  try:
-#    selected_columns=["marketplace","customer_id","review_id","product_id","product_parent","product_title","product_category","star_rating","helpful_votes","total_votes","vine","verified_purchase","review_headline","review_body","review_date","sentiment"]
-#    schema=StructType([
-#    StructField("marketplace",StringType(),True),
-#    StructField("customer_id",StringType(),True),
-#    StructField("review_id",StringType(),True),
-#    StructField("product_id",StringType(),True),
-#    StructField("product_parent",StringType(),True),
-#    StructField("product_title",StringType(),True),
-#    StructField("product_category",StringType(),True),
-#    StructField("star_rating",StringType(),True),
-#    StructField("helpful_votes",StringType(),True),
-#    StructField("total_votes",StringType(),True),
-#    StructField("vine",StringType(),True),
-#    StructField("verified_purchase",StringType(),True),
-#    StructField("review_headline",StringType(),True),
-#    StructField("review_body",StringType(),True),
-#    StructField("review_date",StringType(),True),
-#    StructField("sentiment",StringType(),True),
-#   ])
-#    df = spark.read.format("csv").option("header", True).schema(schema).load(csv_path)
-#    df = df.select(*selected_columns)
-   
-#  except:  # Catch any errors during reading with schema
-#     pass
+string_df = df.select(concat_ws(",", col("marketplace"), col("customer_id"),col("review_id"), col("product_id"),col("product_parent"), col("product_title"),col("product_category"),col("star_rating"), col("helpful_votes"),col("total_votes"),col("vine"),col("verified_purchase"),col("review_headline"),col("review_body"),col("review_date"),col("sentiment")).alias("value"))
 
-df.writeStream\
+#writing to kafka
+    
+string_df.write\
     .format("kafka") \
     .option("kafka.bootstrap.servers", bootstrap_servers) \
     .option("topic", topic ) \
